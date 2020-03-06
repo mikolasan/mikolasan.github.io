c996c2cec3f5590d07146ae0b83f2dc53e46f16b
Nikolay Neupokoev <neupokoev.n@gmail.com>
2020-03-05 22:14:45 -0800

    Clean and add more projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,29 +5,14 @@ module.exports = {
     author: `@mikolasan`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/markdown`,
         name: `markdown-pages`,
+        path: `${__dirname}/src/markdown`,
       },
     },
-    `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-mikolasan-blog`,
-        short_name: `blog`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
     `gatsby-plugin-sass`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
