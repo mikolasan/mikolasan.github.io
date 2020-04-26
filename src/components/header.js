02e053f4fc1718f314f0da768ad306cde3f910fe
Nikolay Neupokoev <neupokoev.n@gmail.com>
2020-04-26 12:04:15 -0700

    Change fonts
    Using Vollkorn (headers), Nunito (paragraph text), Manrope (navbar)

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import NavigationBar from "../components/navigationBar"
 
 const Header = () => (
   <header>
-    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Vollkorn+SC:wght@700&family=Manrope:wght@300&family=Nunito:wght@300&display=swap" rel="stylesheet"></link>
     <NavigationBar />
   </header>
 )
