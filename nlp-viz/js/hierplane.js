84b710a20cab58361b85d4b36eaf080c4cb48ed9
Nikolay Neupokoev <neupokoev.n@gmail.com>
2021-03-27 21:25:25 +0700

    Updates

---
+++ nlp-viz/js/hierplane.js
@@ -0,0 +1,40 @@
+const tree = {
+  text: 'Sam likes boats',
+  root: {
+    nodeType: 'event',
+    word: 'like',
+    spans: [
+      {
+        start: 4,
+        end: 9
+      }
+    ],
+    children: [
+      {
+        nodeType: 'entity',
+        word: 'Sam',
+        link: 'subject',
+        attributes: [ 'Person' ],
+        spans: [
+          {
+            start: 0,
+            end: 3
+          }
+        ]
+      },
+      {
+        nodeType: 'entity',
+        word: 'boat',
+        link: 'object',
+        attributes: [ '>1'],
+        spans: [
+          {
+            start: 10,
+            end: 17
+          }
+        ]
+      }
+    ]
+  }
+};
+hierplane.renderTree(tree);
