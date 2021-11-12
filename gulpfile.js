73ff6490c68eb7ee6bc0a824978748eb33154c34
Nikolay Neupokoev <neupokoev.n@gmail.com>
2021-11-13 00:40:35 +0700

    Read files, find frontmatter, parse it

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,77 @@
 const { src, dest } = require('gulp')
-const through = require('through2')
 const PluginError = require('plugin-error')
+const Transform = require('stream').Transform;
 
 const PLUGIN_NAME = 'gulp-lastmodifiedtime';
 
-function prefixStream(prefixText) {
-  var stream = through();
-  // catch errors from the streamer and emit a gulp plugin error
-  stream.on('error', () => { this.emit('error', new PluginError(PLUGIN_NAME, 'Streamer error')) });
-  let chunk;
-  // Use a loop to make sure we read all currently available data
-  while (null !== (chunk = stream.read())) {
-    console.log(`Read ${chunk.length} bytes of data...`);
-  }
-  // stream.write(prefixText);
-  return stream;
-}
+// https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/dealing-with-streams.md
+// https://github.com/lazd/gulp-replace/blob/master/index.js
+// https://github.com/eugeneware/replacestream/blob/master/index.js
 
-function addLastModifiedTime(prefixText) {
-  if (!prefixText) {
-    throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
-  }
+function addLastModifiedTime(options) {
+  options = Object.assign({
+    encoding: 'utf8',
+  }, options);
 
-  prefixText = new Buffer.from(prefixText); // allocate ahead of time
+  return new Transform({
+    objectMode: true,
+    transform(file, enc, callback) {
+      if (file.isNull()) {
+        return callback(null, file);
+      }
 
-  const stream = through.obj(function(file, enc, cb) {
-    console.log(file.contents)
-    if (file.isBuffer()) {
-      this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
-      return cb();
+      if (file.isBuffer()) {
+        this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
+        return callback();
+      }
+      
+      let noDate = false;
+      if (file.isStream()) {
+        // define the streamer that will transform the content
+        var streamer = new Transform({
+          transform(buf, enc, callback) {
+            const chunk = buf.toString(options.encoding);
+            const re = new RegExp(/---(.*?)---.*/s); // *? - non-greedy
+            const found = chunk.match(re);
+            if (found) {
+              const frontmatter = found[1];
+              const lines = frontmatter.split(/[\r\n]+/);
+              const data = lines.filter(line => line.length > 0).map(line => line.split(':'))
+              if (!data.find(e => e[0] === "date")) {
+                console.log(data);
+                noDate = true;
+              }
+            } 
+            callback(null, chunk)
+
+          }
+        });
+        // catch errors from the streamer and emit a gulp plugin error
+        // streamer.on('error', () => { this.emit('error', new PluginError(PLUGIN_NAME, 'Streamer error')) });
+        // let chunk = streamer.read();
+        // console.log(chunk)
+        // streamer.write( new Buffer.from('test'))
+        // Use a loop to make sure we read all currently available data
+        // while (null !== (chunk = streamer.read())) {
+        //   console.log(`Read ${chunk.length} bytes of data...`);
+        // }
+        // start the transformation
+        file.contents = file.contents.pipe(streamer);
+      }
+
+      // make sure the file goes through the next gulp plugin
+      if (noDate) {
+        this.push(file);
+      }
+      // tell the stream engine that we are done with this file
+      callback(null, file);
     }
-    
-    if (file.isStream()) {
-      // define the streamer that will transform the content
-      var streamer = prefixStream(prefixText);
-      // start the transformation
-      file.contents = file.contents.pipe(streamer);
-    }
-
-    // make sure the file goes through the next gulp plugin
-    this.push(file);
-    // tell the stream engine that we are done with this file
-    cb();
   })
-
-  return stream;
 }
 
 function markdownNoDate(cb) {
   return src('src/markdown/**/*.md', { buffer: false })
-    .pipe(addLastModifiedTime('test'))
+    .pipe(addLastModifiedTime())
     .pipe(dest('noDate'))
 }
 
