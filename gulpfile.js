76dfea73a3cafd3e0b255c0ba94b22b463686585
Nikolay Neupokoev <neupokoev.n@gmail.com>
2021-11-15 23:04:36 +0700

    Sunday

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest } = require('gulp')
 const PluginError = require('plugin-error')
 const Transform = require('stream').Transform;
+const { exec } = require('child_process');
 
 const PLUGIN_NAME = 'gulp-lastmodifiedtime';
 
@@ -8,10 +9,15 @@ const PLUGIN_NAME = 'gulp-lastmodifiedtime';
 // no use of through2, use Transform https://github.com/lazd/gulp-replace/blob/master/index.js
 // get file content https://github.com/eugeneware/replacestream/blob/master/index.js
 // https://github.com/sindresorhus/gulp-filter/blob/main/index.js
+// last modified time https://stackoverflow.com/questions/4784575/how-do-i-find-the-most-recent-git-commit-that-modified-a-file
+// first published time https://stackoverflow.com/questions/11533199/git-find-commit-where-file-was-added
+// git log https://git-scm.com/docs/git-log
+// gulp git https://github.com/stevelacy/gulp-git/blob/master/lib/exec.js
 
 function addLastModifiedTime(options) {
   options = Object.assign({
     encoding: 'utf8',
+    gitRoot: process.cwd(),
   }, options);
 
   return new Transform({
@@ -23,7 +29,6 @@ function addLastModifiedTime(options) {
       }
 
       if (file.isBuffer()) {
-        // this.emit('error', new PluginError(PLUGIN_NAME, 'Buffers not supported!'));
         const chunk = file.contents.toString(options.encoding);
         const re = new RegExp(/---(.*?)---.*/s); // *? - non-greedy
         const found = chunk.match(re);
@@ -32,42 +37,59 @@ function addLastModifiedTime(options) {
           const lines = frontmatter.split(/[\r\n]+/);
           const data = lines.filter(line => line.length > 0).map(line => line.split(':'));
           if (!data.find(e => e[0] === "date")) {
-            console.log(data);
-            return callback(null, file);
+            let path = file.path;
+            if (path.includes(options.gitRoot)) {
+              path = path.substring(options.gitRoot.length + 1);
+            }
+            // console.log(data);
+            // console.log(path);
+
+            const gitLogFirst = exec(`git log --pretty=format:%as --diff-filter=A -- ${path}`, function (error, stdout, stderr) {
+              if (error) {
+                console.log(error.stack);
+                console.log('Error code: '+error.code);
+                console.log('Signal received: '+error.signal);
+                this.emit('error', new PluginError(PLUGIN_NAME, 'git error'));
+                return callback();
+              }
+              console.log('Child Process STDOUT: '+stdout);
+              const published = stdout;
+              const file_1 = file
+              const gitLogLast = exec(`git log -n 1 --pretty=format:%as -- ${path}`, function (error, stdout, stderr) {
+                if (error) {
+                  console.log(error.stack);
+                  console.log('Error code: '+error.code);
+                  console.log('Signal received: '+error.signal);
+                  this.emit('error', new PluginError(PLUGIN_NAME, 'git error'));
+                  return callback();
+                }
+                console.log('Child Process STDOUT: '+stdout);
+                const lastModified = stdout;
+                // chunk += new Buffer.from(lastModified);
+                // console.log('Child Process STDERR: '+stderr);
+                
+                console.log(file_1.path)
+                return callback(null, file_1);
+              });
+
+            });
+            
+            // ls.on('exit', function (code) {
+            //   console.log('Child process exited with exit code '+code);
+            // });
+            
+            // return callback(null, file);
           }
-        }
-        return callback();
-        
+          return;
+        } else {
+          this.emit('error', new PluginError(PLUGIN_NAME, `No frontmatter found in ${file.path}`));
+          return callback();
+        }        
       }
       
       if (file.isStream()) {
         this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
         return callback();
-        // // define the streamer that will transform the content
-        // var streamer = new Transform({
-        //   transform(buf, enc, done) {
-        //     const chunk = buf.toString(options.encoding);
-        //     const re = new RegExp(/---(.*?)---.*/s); // *? - non-greedy
-        //     const found = chunk.match(re);
-        //     if (found) {
-        //       const frontmatter = found[1];
-        //       const lines = frontmatter.split(/[\r\n]+/);
-        //       const data = lines.filter(line => line.length > 0).map(line => line.split(':'));
-        //       if (!data.find(e => e[0] === "date")) {
-        //         console.log(data);
-        //         noDate = true;
-        //         done(null, buf);
-        //         return;
-        //       }
-        //     }
-        //     done();
-        //   }
-        // });
-        // // catch errors from the streamer and emit a gulp plugin error
-        // streamer.on('error', () => { this.emit('error', new PluginError(PLUGIN_NAME, 'Streamer error')) });
-        // streamer.on('data', (chunk) => {console.log(`get chunk`);});
-        // // start the transformation
-        // file.contents = file.contents.pipe(streamer);
       }
       
       callback();
@@ -92,6 +114,20 @@ function markdownNoDate(cb) {
     .pipe(addLastModifiedTime())
     .pipe(justCheckingWorker('just checking 1'))
     // .pipe(justCheckingWorker('just checking 2'))
+    .pipe(dest('noDate'))
+}
+
+
+function markdownAddPublished(cb) {
+  return src('src/markdown/**/*.md', { buffer: true })
+    .pipe(filterByFrontmatterField("published", { notFound: true}))
+    .pipe(addPublishedGitTime())
+    .pipe(dest('noDate'))
+}
+
+function markdownUpdateLastModified(cb) {
+  return src('src/markdown/**/*.md', { buffer: true })
+    .pipe(setLastModifiedGitTime())
     .pipe(dest('noDate'))
 }
 
--- src/components/slots/reels.js
+++ src/components/slots/reels.js
@@ -121,15 +121,31 @@ const symbol_names = new Map([
   [RD, 'Redis'],
 ]);
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function shuffle(array) {
+  let {length} = array;
+  while (length--) {
+      let i = getRandomIntInclusive(0, length);
+      [array[i], array[length]] = [array[length], array[i]];
+  }
+  return array;
+};
 
 const choose_5 = [TW, MA, BS, FO, RE, VU, SV, NG, JQ, JS, PY, SO, CS, JA, PH, RB, QL, RS, MY, SQ, MO, RD];
 const strips = [
-  choose_5,
-  choose_5,
-  choose_5,
-  choose_5,
-  choose_5
+  shuffle(choose_5),
+  shuffle(choose_5),
+  shuffle(choose_5),
+  shuffle(choose_5),
+  shuffle(choose_5),
 ]
+
 // const strips = [
 //   [CC, JS, GO, JA, CC, CC, RB, JS, RU, CC, JA, SC, PY, RU, RB],
 //   [RE, RE, RE, NG, VU, NE, QT, NG, AN, IO, VU, QT],
