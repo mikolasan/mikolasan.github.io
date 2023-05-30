e1a4ba044c6dbfa4425ad293bb0e9921231c7fa0
Nikolay Neupokoev <neupokoev.n@gmail.com>
2023-05-29 22:25:09 -0700

    div to button (a11y)

--- src/components/mobileNav.js
+++ src/components/mobileNav.js
@@ -6,13 +6,13 @@ import * as styles from "./navigationBar.module.css"
 
 const MobileNav = ({ language, menuOpen, menuClickedCallback }) => (
   <nav className={styles.mobilenav}>
-    <div className={[styles.burgermenu, menuOpen && styles.burgermenuopen || ""].join(" ")} onClick={menuClickedCallback}>
+    <button className={[styles.burgermenu, menuOpen && styles.burgermenuopen || ""].join(" ")} onClick={menuClickedCallback}>
       <svg id="burgericon" className={menuOpen && "open" || ""} viewBox="0 0 100 100" width="100%" height="100%">
         <rect className="frstbar" y="10" width="100" height="20"></rect>
         <rect className="scndbar" y="40" width="100" height="20"></rect>
         <rect className="thrdbar" y="70" width="100" height="20"></rect>
       </svg>
-    </div>
+    </button>
     <div className={styles.logobutton}>
       <Link to={language === "en" ? "/" : "/ru"}>
         <LogoButton style={styles.mobilelogo} />
